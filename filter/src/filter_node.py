d97d09d9f74e5f7490085f4d1b215686806667c7
cabraile <cafilho99@gmail.com>
2019-07-17 13:45:34 -0300

    Checkpoint. Added Gazebo workspace.

--- filter/src/filter_node.py
+++ filter/src/filter_node.py
@@ -6,17 +6,20 @@ from enum import Enum
 
 class FilterNode():
 
-    class Index(Enum):
+    class INDICES(Enum):
         LABEL = 0
         MEAS_DISTANCE = 1
+        HEADING_ANGLE = 2
 
-    def velocityCallback(self, data):
-        
+    def velocityCallback(self, velocity):
+        self.filter.predict(velocity)
         return
 
     def landmarkCallback(self, data):
-        label = data[Index.LABEL]
-        dist = data[Index.MEAS_DISTANCE]
+        label = data[INDICES.LABEL]
+        dist = data[INDICES.MEAS_DISTANCE]
+        psi = data[INDICES.HEADING_ANGLE]
+        self.filter.update(label,dist,psi)
         return
 
     def run(self):
@@ -24,6 +27,10 @@ class FilterNode():
         return
 
     def __init__(self):
+        # Inner modules init
+        self.filter = Filter() # TODO: ADD PARAMETRIZATION CONF
+
+        # ROS init
         rospy.init_node("filter_node", anonymous=False)
         rospy.Subscriber("robot_msgs/velocity", Float64, self.velocityCallback)
         rospy.Subscriber("robot_msgs/landmark", Float64MultiArray, self.landmarkCallback)
